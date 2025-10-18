d1447b75531c170f0f1bc306385aa385e5a57069
DIEGO aranda diaz <diegoish20@gmail.com>
2025-10-18 10:13:38 -0600

    Macros

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+@app.route('/index')
+def index():
+    titulo = "Pagina de inicio"
+    listado = ['python', 'Flask', 'Jinja2', 'HTML', 'CSS']
+    return render_template('index.html', titulo = titulo, listado = listado)
+
+@app.route('/distancia')
+def distancia():
+    return render_template('index.html')
+
+@app.route('/calculos')
+def calculos():
+    return render_template('calculo.html')
 
 @app.route('/')
 def home():
@@ -34,6 +48,22 @@ def func1(n1,n2):
 @app.route("/default/<string:dft>")
 def func2(dft="sss"):
     return "El valor de dft es:" + dft
+
+@app.route("/prueba")
+def func4():
+    return '''
+    <html>
+    <head>
+    <title> Página de prueba</title>
+        </head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+        <body>
+            <h1>PAgina de prueba</h1>
+          <p>Esta es una pagina de prueba</p>  
+        <body>
+        </html>
+
+        '''
 
 
 if __name__ == '__main__':
